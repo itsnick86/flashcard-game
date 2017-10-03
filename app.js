9d4e3519818b2db849ef162cc0fc5ccf175a9d02
Nick Edwards <nledwa01@gmail.com>
2017-10-03 12:59:25 -0400

    After a student enters their name into the form they are now redirected to the index view.

--- app.js
+++ app.js
@@ -10,7 +10,8 @@ app.use(cookieParser());
 app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
-  res.render('index');
+  const name = req.cookies.username
+  res.render('index', { name });
 });
 
 app.get('/cards', (req, res) => {
@@ -18,12 +19,12 @@ app.get('/cards', (req, res) => {
 });
 
 app.get('/hello', (req, res) => {
-  res.render('hello', { name: req.cookies.username });
+  res.render('hello');
 });
 
 app.post('/hello', (req, res) => {
   res.cookie('username', req.body.username);
-  res.render('hello', { name: req.body.username });
+  res.redirect('/');
 });
 
 app.listen(3000, () => {
