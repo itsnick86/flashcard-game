32da7868641edfda4ea649a29889cbb95f8e743a
Nick Edwards <nledwa01@gmail.com>
2017-10-02 22:16:36 -0400

    Added a GET and POST route for the new hello form view.

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false}));
 
 app.set('view engine', 'pug');
 
@@ -10,6 +13,15 @@ app.get('/', (req, res) => {
 
 app.get('/cards', (req, res) => {
   res.render('card', { prompt: "Who is buried in Grant's tomb?"});
+});
+
+app.get('/hello', (req, res) => {
+  res.render('hello');
+});
+
+app.post('/hello', (req, res) => {
+  console.dir(req);
+  res.render('hello');
 });
 
 app.listen(3000, () => {
