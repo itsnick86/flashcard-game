82f8251e3e40063d2ded3a611b3152bb5a85cd45
Nick Edwards <nledwa01@gmail.com>
2017-10-05 20:06:45 -0400

    Added data.json file and set up cards.js to read the data.

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-// router.use((req, res, next) => {
-//   console.log("Hello");
-//   next();
-// });
-//
-// router.use((req, res, next) => {
-//   console.log("World");
-//   next();
-// });
 
 router.get('/', (req, res) => {
   const name = req.cookies.username
